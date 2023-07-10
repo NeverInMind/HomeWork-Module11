a011f90fca058ee845f022d636f811764da4bce8
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-07-10 22:07:41 +0300

    Update class.py

--- class.py
+++ class.py
@@ -1,43 +1,32 @@
 from collections import UserDict
-    
-class Record():
-    def add(*args):
-        name = Name(args[0])
-        phone = Phone(args[1])
-        rec = Record(name, phone)
-        return AddressBook.add_record(rec)
-
-    def edit(*args):
-        name = args[0]
-        phone = args[1]
-        rec = AddressBook.data.get(name)
-        if rec:
-            rec['phone'] = phone
-            return rec
-
-
-    def delete(*args):
-        name = args[0]
-        rec = AddressBook.data.get(name)
-        del rec
 
 class Field():
     pass
 
-class Name(Field):
-    pass
+class Name():
+    def __init__(self, name) -> None:
+        self.name = name
 
-class Phone(Field):
-    pass
+class Phone():
+    def __init__(self, phone) -> None:
+        self.phone = phone
+    
+class Record():
+    def __init__(self, *args) -> None:
+        self.name = args[0].name
+        self.phone = args[1].phone
+
+        
 
 class AddressBook(UserDict):
+    
     def add_record(self, rec:Record):
-        self.data[rec.value.name] = rec
+        self.data[rec.name] = rec.phone
 
 
 ab = AddressBook()
 name = Name("Bill")
-phone = Phone("123456")
+phone = Phone("123456",'1232355')
 rec = Record(name, phone)
 ab.add_record(rec)
 print(ab)
