5d560304e7d313b2a38cdb8e27a54607adaf752b
v.dunkan <v.dunkan@gmail.com>
2023-07-10 23:22:48 +0300

    by Skiff

--- class.py
+++ class.py
@@ -1,27 +1,39 @@
 from collections import UserDict
 
-class Field():
-    pass
+class Field:
+    def __init__(self, value) -> None:
+        self.value = value
+    
+    def __str__(self) -> str:
+        return self.value
 
-class Name():
-    def __init__(self, name) -> None:
-        self.name = name
 
-class Phone():
-    def __init__(self, phone) -> None:
-        self.phone = phone
+class Name(Field):
+    ...
+
+
+class Phone(Field):
+    ...
+    
     
 class Record():
-    def __init__(self, *args) -> None:
-        self.name = args[0].name
-        self.phone = args[1].phone
+    def __init__(self, name:Name, phone:Phone=None) -> None:
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone)
+    
+    def __str__(self) -> str:
+        return f"{self.name} : {', '.join(str(p) for p in self.phones)}"
 
         
-
 class AddressBook(UserDict):
     
     def add_record(self, rec:Record):
-        self.data[rec.name] = rec.phone
+        self.data[rec.name] = rec
+    
+    def __str__(self) -> str:
+        return "\n".join(str(r) for r in self.values())
 
 
 ab = AddressBook()
