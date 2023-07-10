7c46b00dc3e1f488369659b0569b5a2e4d623600
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-07-10 20:51:26 +0300

    edit

--- bot.py
+++ bot.py
@@ -1,39 +1,3 @@
-from collections import UserDict
-    
-class Record():
-    def add(*args):
-        name = Name(args[0])
-        phone = Phone(args[1])
-        rec = Record(name, phone)
-        return AddressBook.add_record(rec)
-
-    def edit(*args):
-        name = args[0]
-        phone = args[1]
-        rec = AddressBook.data.get(name)
-        if rec:
-            rec['phone'] = phone
-            return rec
-
-
-    def delete(*args):
-        name = args[0]
-        rec = AddressBook.data.get(name)
-        del rec
-
-class Field():
-    pass
-
-class Name(Field):
-    pass
-
-class Phone(Field):
-    pass
-
-class AddressBook(UserDict):
-    def add_record(self, rec:Record):
-        self.data[rec.value.name] = rec
-
 MEMORY = {}
 
 
